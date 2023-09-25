1329b90d6751beef1d698835257d54bd9a27b786
prateek <Kesharwani3899@gmail.com>
2023-09-25 23:52:25 +0530

    DetailsBanner

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './style.scss'
+import Usefetch from '../../hooks/UseFetch'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailsBanner/DetailsBanner'
 
 export default function Details() {
+  const {mediaType,id}=useParams()
+  const {data,loading }=Usefetch(`/${mediaType}/${id}/videos`)
+  const {data:credits,loading: creditsLoading}=Usefetch(`/${mediaType}/${id}/credits`)
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner video={data?.results[0]} crew={credits?.crew}/>
+    </div>
   )
 }
